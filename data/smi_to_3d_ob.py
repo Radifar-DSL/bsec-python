cf80323eda4218edc45433537a4cd90de7bc2ecd
radifar <m.radifar05@gmail.com>
2020-10-02 14:12:20 +0700

    add molecule files in multiple formats + the script

---
+++ data/smi_to_3d_ob.py
@@ -0,0 +1,19 @@
+from openbabel import openbabel as ob
+
+formats = ["mol", "mol2", "pdb", "sdf", "xyz"]
+
+converter = ob.OBConversion()
+converter.SetInFormat("smi")
+
+mol = ob.OBMol()
+builder = ob.OBBuilder()
+
+not_at_end = converter.ReadFile(mol, "aminoacids.smi")
+while not_at_end:
+    builder.Build(mol)
+    name = mol.GetTitle()
+    for format in formats:
+        converter.SetOutFormat(format)
+        converter.WriteFile(mol, f"aa_ob/{format}/{name}.{format}")
+    mol = ob.OBMol()
+    not_at_end = converter.Read(mol)    
